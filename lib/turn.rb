34daa963f0d895f4e78f39da7e88b9a49a2d53af
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-06-12 18:11:52 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,10 @@
-def display_board (board, position)
-  board=
-  
+def display_board (board)
+  puts " #{board[0]} | #{board[1]} |  #{board[2]}"
+  puts "---------"
+  puts " #{board[3]} | #{board[4]} |  #{board[5]}"
+  puts "---------"
+  puts " #{board[6]} | #{board[7]} |  #{board[8]}"
+
 end
   
   

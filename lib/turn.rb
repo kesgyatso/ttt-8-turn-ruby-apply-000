095c24f954318b56a5bea25275fe7b1455ddee30
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-06-12 17:09:19 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+#ask for input
+#get input
+#convert input to index
+#if index is valid,
+  #make the move for index
+  #show the board
+#else
+  #ask for input again
+#until you get a valid input
+#end
+  
+
+
+
+
+
+
+
+
+end

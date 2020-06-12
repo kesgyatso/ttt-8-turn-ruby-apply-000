e3541d4a54e7b137394da1739d4101cfa59fba25
Kelsang Gyatso <kelsanggyatso@gmail.com>
2020-06-12 18:26:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,51 +1,49 @@
-#display tictactoe board
-def display_board (board)
-  puts " #{board[0]} | #{board[1]} |  #{board[2]}"
-  puts "---------"
-  puts " #{board[3]} | #{board[4]} |  #{board[5]}"
-  puts "---------"
-  puts " #{board[6]} | #{board[7]} |  #{board[8]}"
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-  
-#ask for input + get input + convert input to index
+
 def input_to_index(user_input)
-  index=user_input.input_to_index
-  index-=1
+  index = user_input.to_i
+  index -= 1
   return index
-end  
-  
-#if index is valid, make the move for index then show board, otherwise ask for input again until you get a valid input
+end
+
+
 def valid_move?(board, index)
   def position_taken?(array, ind)
     if array[ind] == " " || array[ind] == "" || array[ind] == nil
-    return false  
-  else
-    return true
+      return false
+    else
+      return true
+    end
   end
-end
 
-def on_board?(num)
+  def on_board?(num)
     if num.between?(0, 8) == true
       return true
     else
       return false
     end
-end
+  end
 
-if (position_taken?(board, index)) == false && (on_board?(index) == true)
+  if (position_taken?(board, index)) == false && (on_board?(index) == true)
     return true
   else
     return false
   end
 end
 
-#Takes a users num and the board, and places the X or O char in that position
+
 def move(board, index, character = "X")
   board[index] = character
   return board
 end
 
-#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
+
 def turn (board)
   puts "Please enter 1-9:"
   num = gets.chomp
@@ -57,4 +55,3 @@ def turn (board)
     turn(board)
   end
 end
-
